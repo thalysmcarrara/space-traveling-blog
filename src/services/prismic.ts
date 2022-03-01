f76332fa2b86111839ceb3c0bf3f7c224cde8154
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-03-01 12:40:48 -0400

    feat: prismic client configured

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,9 +1,8 @@
-import Prismic from '@prismicio/client';
-import { DefaultClient } from '@prismicio/client/types/client';
+import { createClient } from '@prismicio/client';
 
-export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    req,
+export function getPrismicClient() {
+  const prismic = createClient(process.env.PRISMIC_API_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
 
   return prismic;
